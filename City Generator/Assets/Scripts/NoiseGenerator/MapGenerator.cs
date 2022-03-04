f5f992adef7d3d07ea7d5d4e03d2f91acbd9f7de
Alberto Barragán <dragris1234@gmail.com>
2022-03-04 19:12:27 +0100

    Added more variables to noise generator

--- City Generator/Assets/Scripts/NoiseGenerator/MapGenerator.cs
+++ City Generator/Assets/Scripts/NoiseGenerator/MapGenerator.cs
@@ -10,12 +10,35 @@ public class MapGenerator : MonoBehaviour
     [Range(0.1f, 20f)]
     public float noiseScale;
 
+    public int octaves;
+    [Range(0,1)]
+    public float persistance;
+    public float lacunarity;
+    public int seed;
+    public Vector2 offset;
+
     public bool autoUpdate;
 
     public void GenerateMap() {
-        float[,] noiseMap = Noise.GenerateNoiseMap (mapWidth, mapHeight, noiseScale);
+        float[,] noiseMap = Noise.GenerateNoiseMap (mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
     
         MapDisplay display = FindObjectOfType<MapDisplay> ();
         display.DrawNoiseMap (noiseMap);
+    }
+
+    void OnValidate() {
+        // Forces values to not drop below 1
+        if (mapWidth < 1) {
+            mapWidth = 1;
+        }
+        if (mapHeight < 1) {
+            mapHeight = 1;
+        }
+        if (lacunarity  < 1) {
+            lacunarity = 1;
+        }
+        if (octaves < 1 ) {
+            octaves = 1;
+        }
     }
 }
